a8f456a87203df0ede3de113971a65389e75129e
Senths02 <sumi.senthinathan@bbc.co.uk>
2021-06-13 17:08:23 +0100

    Home page using layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,29 @@
 import * as React from "react"
+import Layout from '../components/layout'
 import InstagramIcon from "../icons/instagram_icon.svg"
 import TwitterIcon from "../icons/twitter_icon.svg"
 import LinkedInIcon from "../icons/linkedin_icon.svg"
-import "../styles/global.scss"
+import "../styles/index.scss"
 
 // markup
 const IndexPage = () => {
   return (
-    <main>
-      <title>Sumi Senthi</title>
-      <h1 className="header">Sumi Senthi</h1>
-      {/* Nav */}
-      <div className="bio">
-        <p>Illustrator + Designer, <br /> exploring the intersections of Art, Data and Technology.</p>
-        <p>Mostly I just like making stuff...</p>
-      </div>
-      <div className="social">
-        <a className="social-icon" href='https://www.instagram.com/sumiscribblebot/'><img src={InstagramIcon} /></a>
-        <a className="social-icon" href='https://twitter.com/SumiSenthi'><img src={TwitterIcon} /></a>
-        <a className="social-icon" href='https://www.linkedin.com/in/sumisenthi/'><img src={LinkedInIcon} /></a>
-      </div>
-      <a className="email" href='mailto:sumisenthiart@gmail.com'>sumisenthiart at gmail dot com</a>
-    </main>
+    <Layout pageTitle="Sumi Senthi">
+      <main>
+        <div className="content">
+          <div className="bio">
+            <p>Illustrator + Designer, <br /> exploring the intersections of Art, Data and Technology.</p>
+            <p>Mostly I just like making stuff...</p>
+          </div>
+          <ul className="social">
+            <li><a className="social-icon" href='https://www.instagram.com/sumiscribblebot/'><img src={InstagramIcon} /></a></li>
+            <li><a className="social-icon" href='https://twitter.com/SumiSenthi'><img src={TwitterIcon} /></a></li>
+            <li><a className="social-icon" href='https://www.linkedin.com/in/sumisenthi/'><img src={LinkedInIcon} /></a></li>
+          </ul>
+          <a className="email" href='mailto:sumisenthiart@gmail.com'>sumisenthiart at gmail dot com</a>
+        </div>
+      </main>
+    </Layout>
   )
 }
 
