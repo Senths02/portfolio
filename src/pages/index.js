701fd5cd31824c905203f8240bebf46082ed7c8b
Senths02 <sumi.senthinathan@bbc.co.uk>
2021-11-27 02:00:42 +0000

    Home page - added profile pic, background and changed bio and contact links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,24 @@
 import * as React from "react"
 import Layout from '../components/layout'
-import InstagramIcon from "../icons/instagram_icon.svg"
-import TwitterIcon from "../icons/twitter_icon.svg"
-import LinkedInIcon from "../icons/linkedin_icon.svg"
 import "../styles/index.scss"
+import profilePic from "../images/sumi_senthi_avatar.png"
 
 // markup
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home">
-      <main>
+      <main className="homeContainer">
+      <div aria-hidden="true" className="homeImage"></div>
         <div className="homeContent">
+        <img alt="Portrait of Sumi with black framed glasses, in ink and pencil crayons" className="profile-picture" src={profilePic}/>
           <div className="bio">
-            <p>Occasionally exploring the intersections of Art, Data and Technology...but mostly I just like making stuff.</p>
+            <p>Currently at the BBC working on UX/UI's, Data Vis, Illustrations, 3D Environments, and Animated Explainers.</p>
           </div>
-          <ul className="social">
-            <li className="social-icon"><a href='https://www.instagram.com/sumiscribblebot/'><img src={InstagramIcon} /></a></li>
-            <li className="social-icon"><a href='https://twitter.com/SumiSenthi'><img src={TwitterIcon} /></a></li>
-            <li className="social-icon"><a href='https://www.linkedin.com/in/sumisenthi/'><img src={LinkedInIcon} /></a></li>
+          <ul className="contact">
+            <li className="contactItem"><a href='mailto:sumisenthiart@gmail.com'>Email</a></li>
+            <li className="contactItem"><a href='https://www.linkedin.com/in/sumisenthi/'>LinkedIn</a></li>
+            <li className="contactItem"><a href='https://www.instagram.com/sumishaped/'>Instagram</a></li>
           </ul>
-          <a className="email" href='mailto:sumisenthiart@gmail.com'>sumisenthiart at gmail dot com</a>
         </div>
       </main>
     </Layout>
