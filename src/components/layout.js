a7af1ca28192364fbdb2affb8d1d0689f6fda578
Senths02 <sumi.senthinathan@bbc.co.uk>
2021-11-27 01:59:29 +0000

    Changed nav to fit on one line on smaller screens

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,31 +1,41 @@
 import * as React from 'react'
-import { Link, useStaticQuery, graphql } from 'gatsby'
+// import { Link } from '@reach/router'
+import { Link, StaticQuery, graphql } from 'gatsby'
 import "../styles/global.scss"
+import siteName from "../images/sumi_senthi.png"
 
-const Layout = ({ pageTitle, children }) => {
-    const data = useStaticQuery(graphql`
-        query {
-            site {
-            siteMetadata {
-                title
-            }
-            }
-        }
-        `)
+
+export default function Layout ({ pageTitle, children }) {
   return (
-    <div>
-      <title>{pageTitle} | { data.site.siteMetadata.title }</title>
-      <Link to="/"><h1 className="header">{ data.site.siteMetadata.title }</h1></Link>
-      {/* Nav */}
-      <nav>
-        <ul className="navContainer">
-          <li className="navItem"><Link to="/illustration">Illustration</Link></li>
-          <li className="navItem"><Link to="/design">Design</Link></li>
-          <li className="navItem"><Link to="/else">&#123;Else&#125;</Link></li>
-        </ul>
-      </nav>
-      {children}
-    </div>
-  )
+    <StaticQuery 
+        query={
+          graphql`
+              {
+                site {
+                  siteMetadata {
+                      title
+                  }
+                }
+              }`
+            }
+        render={ data => (
+          <div>
+            <title>{pageTitle} | { data.site.siteMetadata.title }</title>
+            {/* Nav */}
+            <nav role="navigation" className="primaryNavigation">
+              <div><Link aria-current="page" className="navItem home" to="/"><img className="logo" alt="" src={siteName}/></Link></div>
+            <span className="vh">Sumisenthi home page</span>
+              <div>
+                <ul className="navigationGroup">
+                  <li className="navItem galleryItem"><Link to="/illustration">Illustration</Link></li>
+                  <li className="navItem galleryItem"><Link to="/design">Design</Link></li>
+                  <li className="navItem galleryItem"><Link to="/else">Else</Link></li>
+                </ul>
+              </div>
+            </nav>
+            {children}
+          </div>
+        )
+        }
+  />)
 }
-export default Layout
