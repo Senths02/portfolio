bee48c6e148926504d02d678da199b9569cb4f95
Senths02 <sumi.senthinathan@bbc.co.uk>
2024-08-25 14:58:59 +0100

    Edits to bio

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,23 +8,20 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Home">
       <main className="homeContainer">
-      {/* <div aria-hidden="true" className="homeImage"></div> */}
         <div className="homeContent">
         <img alt="Sketch of Sumi with black framed glasses, in ink and pencil crayons" className="profile-picture" src={profilePic}/>
           <div className="bio">
             <p>Hello,<br/>I'm a designer + illustrator</p> 
             
             <hr aria-hidden="true" className="solid"></hr>
-            <p>After working on digital experiences for over 8 years i've learnt to value:</p>
-            <p>Empathy-driven approaches <br/>for long-term solutions</p>
-            <p>Being flexible with <br/> tools and technologies</p>
-            <p>A playful ethos for innovation</p>
+            <p>After working on digital experiences for over 8 years, i've learnt to value:</p> 
+            <p>Human-centred approaches <br /> for effective solutions</p>
+            <p>Play, as a driver for <br /> creativity and innovation</p>
             <p>🐙</p>
-            <p>I also love learning new things.</p>
-            <p>I'm currently learning about the natural sciences, and the impact on technology.</p>
+            <p>I have worked on:</p>
+            <p>Web, mobile, and TV apps, explainer animations, and data visualisations</p>
           </div>
           <hr aria-hidden="true" className="solid"></hr>
-          <p>Open to work:</p> 
           <ul className="contact">
             <li className="contactItem"><a href='mailto:sumi.senthinathan@gmail.com'>Email</a></li>
             <li className="contactItem"><a href='https://www.linkedin.com/in/sumisenthi/'>LinkedIn</a></li>
